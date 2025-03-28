6645fd654f4f87d119b4483664fefe2110574bc9
SudoCpp <SudoCpp@gmail.com>
2025-03-28 13:37:35 -0500

    Add 3-Hanging Processors.

---
+++ 3-Hanging Processors/boot.s
@@ -0,0 +1,59 @@
+
+; Copyright 2025 SudoCPP
+
+; Permission is hereby granted, free of charge, to any person obtaining a copy
+; of this software and associated documentation files (the “Software”), to deal
+; in the Software without restriction, including without limitation the rights
+; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+; of the Software, and to permit persons to whom the Software is furnished to do;
+; so, subject to the following conditions:
+
+; The above copyright notice and this permission notice shall be included in all
+; copies or substantial portions of the Software.
+
+; THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+; INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+; PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+; HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+; OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+; SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+; ================================================================================================= ;
+;                                      Hanging CPUs                                                 ;
+; ------------------------------------------------------------------------------------------------- ;
+; Designed for: Raspberry Pi 4 B                                                                    ;
+; Processor: ARM Cortex-A72                                                                         ;
+; ARM Instructions: https://developer.arm.com/documentation/dui0801/l?lang=en                       ;
+; Technical Ref Manual: https://developer.arm.com/documentation/100095/latest/                      ;
+; Peripherals Manual: https://datasheets.raspberrypi.com/bcm2711/bcm2711-peripherals.pdf            ;
+; ================================================================================================= ;
+
+; ================================================================================================= ;
+;                                   Set up the Assembler
+; ------------------------------------------------------------------------------------------------- ;
+use64                   ; 64-Bit Code                                                               ;
+processor cpu64_v8      ; Raspberry PI 4 Processor Type                                             ;
+; ================================================================================================= ;
+
+org 0x80000             ; Where our code is loaded into RAM. (0x8000 for 32-bit)
+
+; ================================================================================================= ;
+;                                Shutdown All But Main Core                                         ;
+; ------------------------------------------------------------------------------------------------- ;
+; Uses: flags, x0                                                                                   ;
+; Returns: void                                                                                     ;
+; Params: void                                                                                      ;
+; Description: Suspends all cores except for Core 0                                                 ;
+; ------------------------------------------------------------------------------------------------- ;
+load_processor_core_id:                                                                             ;
+    mrs x0, MPIDR_EL1   ; https://developer.arm.com/documentation/100095/0003/System-Control/AArch64-register-descriptions/Multiprocessor-Affinity-Register--EL1?lang=en
+    and x0, x0, #0x3    ; x0 = x0 & 3 which gives us our code ID by itself                          ;
+    cbz x0, main        ; goto main if core ID is 0                                                 ;
+    hang_cpu:                                                                                       ;
+        wfe             ; https://developer.arm.com/documentation/ka001283/latest/                  ;
+    b hang_cpu                                                                                      ;
+; ================================================================================================= ;
+
+main:
+
+b main                  ; Put in loop so it doesn't just start trying to run values in RAM
